e7adecdef78d407f3dcc130427f45d649e74ac7a
stefffffffffano <s323195@studenti.polito.it>
2025-07-07 15:12:25 +0200

    Updated image and personal info

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,9 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   const doctorInfo = {
-    name: "Dott.ssa Elisabetta Fumero, specialista in ostetricia e ginecologia",
+    name: "Dott.ssa Elisabetta Fumero",
+    work_info: "Medico Chirurgo - Specialista Ginecologia ed Ostetricia",
+    work_pos: "Dirigente SC Ostetricia e Ginecologia Ospedale Ciriè (TO)",
     email: "elisabetta.fumero@gmail.com",
     phone: "+39 3273765908",
     location: "Via Torino 31, Nole, Torino",
@@ -26,16 +28,24 @@ const HomePage = () => {
 
         <div className="home-info">
           <h1 className="home-title">{doctorInfo.name}</h1>
+
+          {/* 👩‍⚕️ Sezione professionale */}
+          <div className="home-profession">
+            <p className="home-detail">👩‍⚕️ {doctorInfo.work_info}</p>
+            <p className="home-detail">🏥 {doctorInfo.work_pos}</p>
+          </div>
+
+          {/* 📧 Contatti */}
           <p className="home-detail">📧 <a href={`mailto:${doctorInfo.email}`} className="home-link">{doctorInfo.email}</a></p>
           <p className="home-detail">📞 <a href={`tel:${doctorInfo.phone}`} className="home-link">{doctorInfo.phone}</a></p>
           <p className="home-detail">📍 {doctorInfo.location}</p>
 
+          {/* 📅 Prenotazione */}
           <a
             href="https://tidycal.com/elisabettafumero/orari-visite"
             className="home-button">
             📅 Prenota ora
           </a>
-
         </div>
       </div>
     </div>
